64c3af6350cff7b0d9df7f7e133c62fbdfc21cef
Kevin Yuan <kevihiiin@users.noreply.github.com>
2020-05-27 01:51:44 +0200

    add copy of old matching

---
+++ backend/apps/matching_old/forms/forms.py
@@ -0,0 +1,242 @@
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Column, HTML, Layout, Row, Submit
+from django import forms
+from django.core.exceptions import ValidationError
+from django.forms import ModelForm
+from django.templatetags.static import static
+from django.utils.translation import gettext_lazy as _
+
+from apps.matching_old.models import User
+from apps.matching_old.models import EmailToHospital
+from apps.matching_old.models import Hospital
+
+# Base hospital Form
+# for signup
+# for saving
+# for editing
+#
+
+
+class HospitalFormO(ModelForm):
+    class Meta:
+        model = Hospital
+        exclude = [
+            "uuid",
+            "registration_date",
+            "user",
+            "sonstige_infos",
+            "max_mails_per_day",
+            "appears_in_map",
+            "approval_date",
+            "approved_by",
+        ]
+
+        labels = {
+            "plz": _("Postleitzahl"),
+            "countrycode": _("Land"),
+            "firmenname": _("Offizieller Name Ihrer Institution"),
+            "ansprechpartner": _("Name der Kontaktperson"),
+            "appears_in_map": _("Auf der Karte sichtbar und kontaktierbar für Helfende sein"),
+            "datenschutz_zugestimmt": _(
+                'Hiermit akzeptiere ich die <a href="/dataprotection/">Datenschutzbedingungen</a>.'
+            ),
+            "einwilligung_datenweitergabe": _(
+                "Ich akzeptiere, dass alle Registrierungen von uns sorgfältig validiert und auf Ihre Richtigkeit kontrolliert werden. Sollte dabei ein Fehler entstehen und Ihre Daten an dritte Personen gelangen, so übernehmen wir keine Haftung dafür."
+            ),
+        }
+
+    def __init__(self, *args, **kwargs):
+        super(HospitalFormO, self).__init__(*args, **kwargs)
+        self.helper = FormHelper()
+        self.helper.form_id = "id-exampleForm"
+        self.helper.form_class = "blueForms"
+        self.helper.form_method = "post"
+        self.helper.form_action = "submit_survey"
+
+        self.helper.layout = Layout(
+            Row(Column("firmenname"), Column("ansprechpartner")),
+            Row(Column("telefon"), Column("email")),
+            Row(Column("plz"), Column("countrycode")),
+            HTML('<hr style="margin-top: 20px; margin-bottom:30px;">'),
+            HTML('<p class="text-left">'),
+            "datenschutz_zugestimmt",
+            HTML("</p>"),
+            HTML('<p class="text-left">'),
+            "einwilligung_datenweitergabe",
+        )
+
+    def clean_datenschutz_zugestimmt(self):
+        if not self.cleaned_data["datenschutz_zugestimmt"]:
+            raise ValidationError(_("Zustimmung erforderlich."), code="invalid")
+        return True
+
+    def clean_einwilligung_datenweitergabe(self):
+        if not self.cleaned_data["einwilligung_datenweitergabe"]:
+            raise ValidationError(_("Zustimmung erforderlich."), code="invalid")
+        return True
+
+
+class HospitalForm(HospitalFormO):
+    def __init__(self, *args, **kwargs):
+        super(HospitalForm, self).__init__(*args, **kwargs)
+        self.helper.add_input(
+            Submit(
+                "submit",
+                "Jetzt registrieren",
+                onclick="this.form.submit(); this.disabled=true; this.value='Sending…';",
+            )
+        )
+
+
+class HospitalFormEditProfile(HospitalFormO):
+    class Meta:
+        model = Hospital
+        exclude = [
+            "uuid",
+            "registration_date",
+            "user",
+            "datenschutz_zugestimmt",
+            "einwilligung_datenweitergabe",
+            "max_mails_per_day",
+            "approval_date",
+            "approved_by",
+        ]
+
+        labels = {
+            "plz": _("Postleitzahl"),
+            "countrycode": _("Land"),
+            "sonstige_infos": _(
+                "Text Ihrer Suchanzeige. Wird nur öffentlich gezeigt wenn Sie auf der Karte sichtbar sind und kontaktbierbar sein möchten. Hier können Sie genau beschreiben, für welche Rollen Sie Unterstützung brauchen, welche Qualifikationen dafür notwendig sind, und unter welchen Bedingungen (z.B. Bezahlung) gesucht wird."
+            ),
+            "firmenname": _("Offizieller Name Ihrer Institution"),
+            "ansprechpartner": _("Name der Kontaktperson"),
+            "appears_in_map": _("Auf der Karte sichtbar und kontaktierbar für Helfende sein"),
+        }
+
+    def __init__(self, *args, **kwargs):
+        super(HospitalFormEditProfile, self).__init__(*args, **kwargs)
+        self.fields["sonstige_infos"].required = False
+        self.helper.add_input(
+            Submit("submit", _("Daten aktualisieren"), css_class="btn blue text-white btn-md",)
+        )
+        self.helper.layout = Layout(
+            Row(Column("firmenname"), Column("ansprechpartner")),  # Row(Column('appears_in_map')),
+            Row(Column("telefon")),
+            Row(Column("plz"), Column("countrycode")),
+        )
+
+
+class HospitalFormZustimmung(ModelForm):
+    class Meta:
+        model = Hospital
+        fields = ["datenschutz_zugestimmt", "einwilligung_datenweitergabe"]
+
+        labels = {
+            "datenschutz_zugestimmt": _(
+                'Hiermit akzeptiere ich die <a href="/dataprotection/">Datenschutzbedingungen</a>.'
+            ),
+            "einwilligung_datenweitergabe": _(
+                "Ich bestätige, dass meine Angaben korrekt sind und ich der Institution meinen Ausbildungsstand nachweisen kann. Mit der Weitergabe meiner Kontaktdaten an die Institutionen bin ich einverstanden."
+            ),
+        }
+
+    def __init__(self, *args, **kwargs):
+        super(HospitalFormZustimmung, self).__init__(*args, **kwargs)
+        self.helper = FormHelper()
+        self.helper.add_input(
+            Submit("submit", _("Daten aktualisieren"), css_class="btn blue text-white btn-md",)
+        )
+        self.helper.layout = Layout(
+            HTML('<p class="text-left">'),
+            "datenschutz_zugestimmt",
+            HTML("</p>"),
+            HTML('<p class="text-left">'),
+            "einwilligung_datenweitergabe",
+        )
+
+    def clean_datenschutz_zugestimmt(self):
+        if not self.cleaned_data["datenschutz_zugestimmt"]:
+            raise ValidationError(_("Zustimmung erforderlich."), code="invalid")
+        return True
+
+    def clean_einwilligung_datenweitergabe(self):
+        if not self.cleaned_data["einwilligung_datenweitergabe"]:
+            raise ValidationError(_("Zustimmung erforderlich."), code="invalid")
+        return True
+
+
+def check_unique_email(value):
+    if User.objects.filter(email=value).exists():
+        raise ValidationError(_("Diese Email ist bereits vergeben"))
+    return value
+
+
+class HospitalFormInfoSignUp(HospitalFormO):
+    email = forms.EmailField(
+        validators=[check_unique_email], label=_("Offizielle E-Mail-Adresse der Kontaktperson"),
+    )
+
+    def __init__(self, *args, **kwargs):
+        super(HospitalFormInfoSignUp, self).__init__(*args, **kwargs)
+        self.helper.form_tag = False
+
+
+class HospitalFormInfoCreate(HospitalFormO):
+    # Used internally to bypass duplicate email validation
+    email = forms.EmailField()
+
+
+class EmailToHospitalForm(forms.ModelForm):
+    class Meta:
+        model = EmailToHospital
+        fields = ["subject", "message"]
+        labels = {"subject": _("Betreff"), "message": _("Nachrichtentext")}
+
+        help_texts = {
+            "message": _(
+                "Gib hier deine Antwort auf das Angebot ein. Wer bist du? Welche Fähigkeiten bringst du für diese Stelle mit?"
+            )
+        }
+
+    def __init__(self, *args, **kwargs):
+        super(EmailToHospitalForm, self).__init__(*args, **kwargs)
+        self.helper = FormHelper()
+        self.helper.form_method = "post"
+        self.helper.add_input(
+            Submit("submit", _("Hilfsangebot abschicken"), css_class="btn blue text-white btn-md",)
+        )
+
+    def clean_message(self):
+        message = self.cleaned_data["message"]
+        initial_message = self.initial["message"]
+        if "".join(str(message).split()) == "".join(str(initial_message).split()):
+            raise ValidationError(_("Bitte personalisiere diesen Text"), code="invalid")
+        return message
+
+
+class PostingForm(forms.ModelForm):
+    class Meta:
+        model = Hospital
+        labels = {
+            "appears_in_map": _("Anzeige soll angezeigt werden"),
+            "sonstige_infos": _("Anzeigetext"),
+        }
+        fields = ["appears_in_map", "sonstige_infos"]
+
+    def __init__(self, *args, **kwargs):
+        super(PostingForm, self).__init__(*args, **kwargs)
+        self.fields["sonstige_infos"].required = False
+        self.helper = FormHelper()
+        self.helper.form_method = "post"
+        self.helper.layout = Layout(
+            HTML(
+                '<script type="text/javascript" src="{}"></script>'.format(
+                    static("js/PostingForm.js")
+                )
+            ),
+            "appears_in_map",
+            "sonstige_infos",
+        )
+        self.helper.add_input(
+            Submit("submit", _("Anzeige aktualisieren"), css_class="btn blue text-white btn-md",)
+        )
---
+++ backend/apps/matching_old/views/student_selection.py
@@ -0,0 +1,103 @@
+import logging
+
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponse
+from django.shortcuts import render
+from django.utils.decorators import method_decorator
+from django.views.generic.base import View
+
+from apps.matching_old.admin import hospital_required
+from apps.matching_old.filters import StudentJobRequirementsFilter
+from apps.matching_old.models import Student
+from apps.matching_old.tables import StudentTable, StudentSelectionTable
+from apps.matching_old.src.map import get_plzs_close_to, plzs
+
+logger = logging.getLogger(__name__)
+
+
+@method_decorator([login_required, hospital_required], name="dispatch")
+class StudentSelectionView(View):
+    def get(self, request, countrycode, plz, distance):
+
+        # remove parameters fro mthe get request that should not be taken into account
+        request_filtered = self.clean_request(request)
+
+        # only show validated students
+        qs = Student.objects.filter(user__validated_email=True, is_activated=True)
+
+        # filter by location
+        countrycode = request.GET.get("countrycode", countrycode)
+        plz = request.GET.get("plz", plz)
+        distance = int(request.GET.get("distance", distance))
+
+        if countrycode not in plzs or plz not in plzs[countrycode]:
+            return HttpResponse(
+                "Postleitzahl: " + plz + " ist keine valide Postleitzahl in " + countrycode
+            )
+        lat, lon, ort = plzs[countrycode][plz]
+        if distance == 0:
+            close_plzs = [plz]
+        else:
+            close_plzs = get_plzs_close_to(countrycode, plz, distance)
+        qs = qs.filter(plz__in=close_plzs, countrycode=countrycode)
+
+        # filter by job requirements
+        filter_jobrequirements = StudentJobRequirementsFilter(request_filtered, queryset=qs)
+        qs = filter_jobrequirements.qs
+
+        # displayed table
+        table = StudentSelectionTable(qs, hospital=request.user.hospital)
+
+        # disable huge amounts of email sends
+        max_mails = request.user.hospital.leftover_emails_for_today()
+        enable_mail_send = filter_jobrequirements.qs.count() <= max_mails
+
+        # special display to enable the fancy java script stuff and logic
+        DISPLAY_filter_jobrequirements = StudentJobRequirementsFilter(
+            request_filtered, display_version=True
+        )
+
+        context = {
+            "plz": plz,
+            "countrycode": countrycode,
+            "ort": ort,
+            "distance": distance,
+            "table": table,
+            "filter": DISPLAY_filter_jobrequirements,
+            "n": qs.count(),
+            "enable_mail": enable_mail_send,
+            "max": max_mails,
+            "email": request.user.email,
+        }
+
+        return render(request, "student_list_view.html", context)
+
+    def clean_request(self, request):
+        keys = list(request.GET.keys())
+        request_filtered = request.GET.copy()
+        for k in keys:
+            if k.startswith("ausbildung_typ_") and k.count("_") > 2:
+                # this is a subfield with the selection notenabled this should not be in the filter
+                # possibly also solvable by javascript (do not send these hidden boxes at all))
+                if not "_".join(k.split("_")[:3]) in request.GET:
+                    request_filtered.pop(k)
+        return request_filtered
+
+    def clean_request_for_saving(self, request):
+        student_attr = dict(request)
+        for i in ["plz", "distance", "countrycode", "uuid", "saveFilter", "filterName"]:
+            if i in request.keys():
+                student_attr.pop(i)
+
+        for i in list(student_attr.keys()):
+
+            if type(student_attr[i]) == list:
+                student_attr[i] = student_attr[i][0]
+
+            if student_attr[i] == "":
+                student_attr.pop(i)
+            elif student_attr[i] == "true":
+                student_attr[i] = True
+            elif student_attr[i] == "false":
+                student_attr[i] = False
+        return student_attr
