501c271379125840803f9d7db0c467e797b51480
Sebastian Bischoff <sebastian.bischoff@student.hpi.de>
2020-06-05 22:10:46 +0200

    Revert wait in deploy.sh and add it for all usage scenarios directly in docker-compose.yml

--- deploy.sh
+++ deploy.sh
@@ -3,11 +3,6 @@ set -e -o pipefail
 # First build containers, compile messages, collect static files (copy them to static_root) and migrate database
 export CURRENT_UID=$(id -u):$(id -g)
 docker-compose -f docker-compose.dev.yml -f docker-compose.prod.yml up -d --build
-echo "Waiting for database"
-while [ $(docker exec match4everything_database_1 psql | grep "Is the server running locally and accepting" | wc -l ) -ge 1 ] || \
-	[ $(docker exec match4everything_database_1 psql | grep "psql: error: could not connect to server: FATAL:  the database system is starting up" | wc -l ) -ge 1 ] ; do
-    sleep 1
-done
 echo "Server started for setup"
 docker exec backend python3 manage.py migrate
 docker exec --env PYTHONPATH="/match4everyone-backend:$PYTHONPATH" backend django-admin makemessages --no-location --ignore 00_old_m4h_matching_code
