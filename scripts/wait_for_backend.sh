940586d3fc482c597b53e58588c94e378031783e
Malte Zacharias <me@malte-zacharias.de>
2020-06-15 06:06:13 +0200

    Fix travis checks (use docker-compose)

--- scripts/wait_for_backend.sh
+++ scripts/wait_for_backend.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-while ! [ $(docker ps | grep backend | wc -l) -gt 0 ]; do
+while ! [ $(docker-compose -f docker-compose.yml -f docker-compose.prod.yml ps | grep backend | wc -l) -gt 0 ]; do
     sleep 1
 done
