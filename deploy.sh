ebdf78a45f0627d19b97d8572a3386c787b3cd1b
Malte Zacharias <me@malte-zacharias.de>
2020-06-15 04:30:48 +0200

    Update scripts
    Forgot to save before commit..

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@
 set -e -o pipefail
 # First build containers, compile messages, collect static files (copy them to static_root) and migrate database
 export CURRENT_UID=$(id -u):$(id -g)
-docker-compose -f docker-compose.dev.yml -f docker-compose.prod.yml up -d --build
+docker-compose -f docker-compose.yml -f docker-compose.prod.yml up -d --build
 echo "Server started for setup"
 docker exec backend python3 manage.py migrate
 docker exec --env PYTHONPATH="/match4everyone-backend:$PYTHONPATH" backend django-admin makemessages --no-location --ignore 00_old_m4h_matching_code
@@ -11,6 +11,6 @@ docker exec backend python3 manage.py collectstatic --no-input
 docker exec backend python3 manage.py migrate
 docker exec backend python3 manage.py check
 # Restart container AFTER static files have been collected
-docker-compose -f docker-compose.dev.yml -f docker-compose.prod.yml down
-docker-compose -f docker-compose.dev.yml -f docker-compose.prod.yml up -d
+docker-compose -f docker-compose.yml -f docker-compose.prod.yml down
+docker-compose -f docker-compose.yml -f docker-compose.prod.yml up -d
 echo "Server is ready for you"
