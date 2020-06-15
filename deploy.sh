de7d7d12dc7eca170cf7b1af3847cea38d089258
Malte Zacharias <me@malte-zacharias.de>
2020-06-15 06:57:03 +0200

    More debugging info in travis

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
-set -e -o pipefail
+set -e -o pipefail -o xtrace
 # First build containers, compile messages, collect static files (copy them to static_root) and migrate database
 export CURRENT_UID=$(id -u):$(id -g)
 export dc="docker-compose -f docker-compose.yml -f docker-compose.prod.yml"
 $dc up -d --build
 echo "Server started for setup"
+$dc exec backend bash -c 'while ! (< /dev/tcp/database/5432) &> /dev/null; do echo "Waiting for database...";sleep 1;done;'
 $dc exec backend python3 manage.py migrate
 $dc exec --env PYTHONPATH="/match4everyone-backend:$PYTHONPATH" backend django-admin makemessages --no-location --ignore 00_old_m4h_matching_code
 $dc exec --env PYTHONPATH="/match4everyone-backend:$PYTHONPATH" backend django-admin compilemessages
