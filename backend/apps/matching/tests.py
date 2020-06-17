f7ddd5f93ca9d805b1bec0c4ecf3cd9db87b6265
Bjarne Sievers <jellyfishcoding@live.de>
2020-06-17 15:20:52 +0200

    Refactor test

--- backend/apps/matching/tests.py
+++ backend/apps/matching/tests.py
@@ -4,11 +4,16 @@ from django.core.management import call_command
 from django.test import TestCase
 
 
+def create_superuser():
+    output = StringIO()
+    args = ["--noinput", "--email=staff@example.com", "--username=staff"]
+    opts = {}
+    call_command("createsuperuser", *args, stdout=output, **opts)
+    return output.getvalue()
+
+
 class ProvenBugsTestCase(TestCase):
     def test_superuser_creation(self):
 
-        output = StringIO()
-        args = ["--noinput", "--email=staff@example.com", "--username=staff"]
-        opts = {}
-        call_command("createsuperuser", *args, stdout=output, **opts)
-        self.assertIn("Superuser created successfully", output.getvalue())
+        output = create_superuser()
+        self.assertIn("Superuser created successfully", output)
