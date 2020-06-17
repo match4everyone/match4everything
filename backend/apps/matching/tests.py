39dc24e2cf45bafe1f3033cb55517b13901102c9
Bjarne Sievers <jellyfishcoding@live.de>
2020-06-17 16:54:09 +0200

    Add test for migration

--- backend/apps/matching/tests.py
+++ backend/apps/matching/tests.py
@@ -1,7 +1,7 @@
 from io import StringIO
 
 from django.core.management import call_command
-from django.test import TestCase
+from django.test import TestCase, TransactionTestCase
 
 
 def create_superuser():
@@ -12,8 +12,45 @@ def create_superuser():
     return output.getvalue()
 
 
+def create_fakeusers(a=10, b=5):
+    output = StringIO()
+    args = ["--add-a=" + str(a), "--add-b=" + str(b)]
+    opts = {}
+    call_command("createfakeusers", *args, stdout=output, **opts)
+    output_string = output.getvalue()
+    assert ("Created " + str(b) + " participants") in output_string
+    return output_string
+
+
 class ProvenBugsTestCase(TestCase):
     def test_superuser_creation(self):
 
         output = create_superuser()
         self.assertIn("Superuser created successfully", output)
+
+
+class MigrationTestCase(TransactionTestCase):
+    def test_migration_with_data(self):
+        # Migrate to starting point
+        output = StringIO()
+        args = ["matching", "0001_initial"]
+        call_command("migrate", *args, stdout=output)
+
+        # Create testdata, otherwise a data migration would be easy
+        create_superuser()
+        create_fakeusers()
+
+        # Test successful migration
+        output = StringIO()
+        call_command("migrate", stdout=output)
+
+        self.assertNotIn("Traceback", output.getvalue())
+        self.assertNotIn("Error", output.getvalue())
+
+        # Test successful rollback
+        output = StringIO()
+        args = ["matching", "0001_initial"]
+        call_command("migrate", *args, stdout=output)
+
+        self.assertNotIn("Traceback", output.getvalue())
+        self.assertNotIn("Error", output.getvalue())
