fbc4a5c1a26ff947e5f3b66eb5ecff6725aac9fc
Jonathan Sauder <sauder@campus.tu-berlin.de>
2020-06-22 23:13:48 +0200

    Add participant view info backend

--- backend/apps/matching/urls.py
+++ backend/apps/matching/urls.py
@@ -14,12 +14,17 @@ urlpatterns = [
     #  User signup/editing/dashboards
     ####################################
     path("<p:p_type>/signup", views.ParticipantSignup.as_view(), name="signup"),
-    path("profile_redirect", views.ProfileDashboardRedirect.as_view(), name="profile_redirect"),
+    path("profile_redirect", views.ProfileDashboardRedirect.as_view(), name="profile_redirect",),
     path("<p:p_type>/profile", views.ParticipantDashboard.as_view(), name="profile"),
     path(
         "<p:p_type>/info/<str:uuid>/edit/",
         views.ParticipantInfoUpdateView.as_view(),
         name="info-edit",
+    ),
+    path(
+        "<p:p_type>/info/<str:uuid>/view/",
+        views.ParticipantInfoViewView.as_view(),
+        name="info-view",
     ),
     path(
         "change_activation",
@@ -78,7 +83,8 @@ urlpatterns = [
     path(
         "password_reset/",
         auth_views.PasswordResetView.as_view(
-            template_name="registration/password_reset_form_.html", from_email=settings.NOREPLY_MAIL
+            template_name="registration/password_reset_form_.html",
+            from_email=settings.NOREPLY_MAIL,
         ),
         name="password_reset_form",
     ),
