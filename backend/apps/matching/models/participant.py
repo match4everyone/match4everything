6827f2c8941026756ee4a4ae42788c1469cca382
Freya <behrefr@gmail.com>
2020-06-23 00:25:39 +0200

    Explicit error msg

--- backend/apps/matching/models/participant.py
+++ backend/apps/matching/models/participant.py
@@ -56,7 +56,7 @@ class AbstractParticipant(models.Model):
         # check permission only here in case it was forgotten in the view
         # maybe we can give a nicer message here?
         if not approver.has_perm("matching.perm_approve_%s"):
-            raise ValueError("not allowed")
+            raise ValueError("You currently don't have the permission to approve users.")
 
         approved_participants = Group.objects.get(name="approved_%s" % p_type)
 
