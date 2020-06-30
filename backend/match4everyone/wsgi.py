24bf04e32f7ef0fc5b7dd78614479aaedf97603f
Sebastian Bischoff <sebastian.bischoff@student.hpi.de>
2020-06-30 16:59:07 +0200

    Use asynchronous calls (psycogreen) for database connections. Unsure if we need it for both, asgi and wsgi

--- backend/match4everyone/wsgi.py
+++ backend/match4everyone/wsgi.py
@@ -10,7 +10,9 @@ https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
+from gevent import monkey
 
+monkey.patch_all()
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "match4everyone.settings.production")
 
 application = get_wsgi_application()
