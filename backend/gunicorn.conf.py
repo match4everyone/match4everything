7dd22e267f9602a3ec5be8c40803cbade6a67a07
Sebastian Bischoff <sebastian.bischoff@student.hpi.de>
2020-06-30 16:59:07 +0200

    Use asynchronous calls (psycogreen) for database connections. Unsure if we need it for both, asgi and wsgi

--- backend/gunicorn.conf.py
+++ backend/gunicorn.conf.py
@@ -7,7 +7,7 @@ bind = "backend:8000"
 workers = multiprocessing.cpu_count() * 2 + 1
 worker_class = "gevent"
 worker_connections = 1000
-timeout = 60
+timeout = 30
 
 accesslog = os.path.join(settings.RUN_DIR, "gunicorn-access.log")
 access_log_format = '%(t)s|"%(r)s"|%(s)s|%(T)s'
