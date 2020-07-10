736927eec6068451902841ee765ebf58cf453121
Bjarne Sievers <jellyfishcoding@live.de>
2020-07-10 17:39:24 +0200

    Also prevent editing of other peoples profile info :D

--- backend/apps/matching/views/participant_info_edit.py
+++ backend/apps/matching/views/participant_info_edit.py
@@ -1,5 +1,6 @@
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
+from django.http import Http404
 from django.urls import reverse
 from django.utils.decorators import method_decorator
 from django.utils.translation import ugettext_lazy as _
@@ -17,6 +18,11 @@ class ParticipantInfoUpdateView(UpdateView):
     template_name = "participant/participant_info_edit_form.html"
     slug_url_kwarg = "uuid"
     slug_field = "uuid"
+
+    def dispatch(self, *args, **kwargs):
+        if self.request.user.participant().info.uuid not in self.request.path:
+            raise Http404
+        return super(ParticipantInfoUpdateView, self).dispatch(*args, **kwargs)
 
     def get_form_class(self):
         return ParticipantEditInfoForm[self.kwargs["p_type"]]
