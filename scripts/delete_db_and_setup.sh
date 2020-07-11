e5b55e32fa31a9be676ce5048877fbb49f49ca0d
Bjarne Sievers <jellyfishcoding@live.de>
2020-07-11 18:43:02 +0200

    Add explanation for user to script

--- scripts/delete_db_and_setup.sh
+++ scripts/delete_db_and_setup.sh
@@ -1,4 +1,6 @@
 rm backend/run/db.sqlite3
 python3 backend/manage.py migrate
+echo "Creating account with username \"admin\". "
+echo "Please enter your desired email and password:"
 python3 backend/manage.py createsuperuser  --username admin
 python3 backend/manage.py loaddata backend/cms_export.json
