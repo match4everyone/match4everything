02a859b2313bde366a043142eeb4efe18bb85fe1
Sebastian Bischoff <sebastian.bischoff@student.hpi.de>
2020-07-18 16:53:57 +0200

    Use db agnostic reset and add possibility to run commands in docker

--- scripts/delete_db_and_setup.sh
+++ scripts/delete_db_and_setup.sh
@@ -1,6 +1,10 @@
-rm backend/run/db.sqlite3
-python3 backend/manage.py migrate
-echo "Creating account with username \"admin\". "
-echo "Please enter your desired email and password:"
-python3 backend/manage.py createsuperuser  --username admin
-python3 backend/manage.py loaddata backend/cms_export.json
+#!/bin/bash
+# To run script in Docker call for example "delete_db_and_setup.sh 'docker-compose exec backend'"
+EXEC_ENV="$*"
+
+$EXEC_ENV python3 manage.py reset_db
+$EXEC_ENV python3 manage.py migrate
+$EXEC_ENV echo "Creating account with username \"admin\". "
+$EXEC_ENV echo "Please enter your desired email and password."
+$EXEC_ENV python3 manage.py createsuperuser  --username admin
+$EXEC_ENV python3 manage.py loaddata cms_export.json
