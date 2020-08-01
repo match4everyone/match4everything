f9cb1bfe481d91d690606b90c9459afbdb6e84cb
Malte Zacharias <me@malte-zacharias.de>
2020-08-01 17:31:08 +0200

    Webpack: Enable source-map creation
    Source-Maps allow developers to see the original js and css code before
    minification and bundling by webpack. This is helpful
    for debugging.

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -100,6 +100,9 @@ module.exports = {
                     loader: 'style-loader', // inject CSS to page
                 }, {
                     loader: 'css-loader', // translates CSS into CommonJS modules
+                    options: {
+                        sourceMap: true,
+                    },
                 }, {
                     loader: 'postcss-loader', // Run post css actions
                     options: {
@@ -111,7 +114,10 @@ module.exports = {
                         }
                     }
                 }, {
-                    loader: 'sass-loader' // compiles Sass to CSS
+                    loader: 'sass-loader', // compiles Sass to CSS
+                    options: {
+                        sourceMap: true,
+                    },
                 }]
             },
             {
@@ -200,4 +206,5 @@ module.exports = {
         }),
         new VueLoaderPlugin(),
     ],
+    devtool: 'eval-source-map',
 }
