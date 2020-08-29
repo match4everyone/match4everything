bf42af67cebb74d14c9389e821355d3fd4d23c11
Malte Zacharias <me@malte-zacharias.de>
2020-08-29 02:48:22 +0200

    New separate Accordion component for Filters

---
+++ frontend/src/components/Accordion.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="vue-accordion">
+    <div class="p-2" @click="toggle">
+      {{ label }}
+      <span v-if="badgeContent === 0 || badgeContent" class="badge badge-primary">{{ badgeContent}}</span>
+      <span class="fa fa-caret-down" style="float:right"></span>
+    </div>
+    <div class="collapse p-2" ref="collapsible">
+      <slot></slot>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  props: {
+    label: String,
+    badgeContent: { },
+  },
+  methods: {
+    toggle() {
+      this.$jQuery(this.$refs.collapsible).collapse('toggle')
+    },
+    show() {
+      this.$jQuery(this.$refs.collapsible).collapse('show')
+    },
+    hide() {
+      this.$jQuery(this.$refs.collapsible).collapse('hide')
+    },
+  }
+}
+</script>
