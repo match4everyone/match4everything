0faf8ff88328d26b477ebc1264717fbffea15306
Malte Zacharias <me@malte-zacharias.de>
2020-09-16 21:47:40 +0200

    add bootstrap-vue

--- frontend/src/filter-ui.js
+++ frontend/src/filter-ui.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import FilterUIComponentDefinition from './components/FilterUI'
 import { FilterComponentManager } from './utils/FilterComponentManager'
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
 
 /*
 Fugly, but need to get the global object, if imported the way it should be
@@ -9,8 +10,12 @@ them, there is no choice
 */
 const globalBootstrapJQuery = window.$
 Vue.prototype.$jQuery = globalBootstrapJQuery
-
 FilterComponentManager.registerComponents(Vue)
+
+// Install BootstrapVue
+Vue.use(BootstrapVue)
+// Optionally install the BootstrapVue icon components plugin
+Vue.use(IconsPlugin)
 
 document.addEventListener('DOMContentLoaded', () => {
     const FilterUI = Vue.extend(FilterUIComponentDefinition)
